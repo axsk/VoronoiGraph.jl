acbd612f32df526f7e92d48e1693716527c64e97
Documenter.jl <documenter@juliadocs.github.io>
2021-10-15 14:17:23 +0000

    build based on 8c849d0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VoronoiGraph","category":"page"},{"location":"#VoronoiGraph","page":"Home","title":"VoronoiGraph","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VoronoiGraph.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VoronoiGraph]","category":"page"}]
+}
