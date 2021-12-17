8033f73095c5194752f00d30636455d7a623155c
Sikorski <sikorski@zib.de>
2021-12-17 10:45:44 +0100

    volume: fix codim>0 error and SVector transformation

--- src/volume.jl
+++ src/volume.jl
@@ -28,7 +28,17 @@ function boundary_area(g1::Int, g2::Int, vertices::AbstractVector{<:Sigma}, gene
     # CDDLib: 40 mins
     # QHull: 5h (with warnings about not affine polyhedron using a solver)
     poly = polyhedron(hrep(halfspaces))
-    vol = hasrays(poly) ? Inf : volume(poly)
+    local vol
+    try
+        vol = hasrays(poly) ? Inf : volume(poly)
+    catch e
+        if isa(e, AssertionError)
+            @warn e
+            vol = NaN
+        else
+            rethrow(e)
+        end
+    end
 
     return vol
 end
@@ -38,10 +48,18 @@ the first dimension. A->B will be mapped to const*[1,0,0,...] and (A+B)/2 to [0,
 function transformation(A, B)
     R = diagm(ones(length(A)))
     R[:,1] = B-A
-    R = inv(qr(R).Q)  # Shortcut for Gram-Schmidt orthogonalization.
+    R = collect(inv(qr(R).Q))  # Shortcut for Gram-Schmidt orthogonalization.
     t = R*(A+B)/2
     transform(x) = R*x - t
     return transform
+end
+
+function transformation(A::SVector{N, T}, B::SVector{N, T}) where {N, T}
+    R = MMatrix{N,N,T}(I)
+    R[:,1] = B-A
+    R = inv(qr(R).Q)
+    t = R * (A+B)/2
+    transform(x) = R*x - t
 end
 
 """ build the connectivity matrix for the SQRA from adjacency and boundary information """
@@ -54,7 +72,7 @@ function volumes(vertices, P::AbstractVector)
     As = Float64[]
     Vs = zeros(length(P))
 
-    @showprogress 1 "Voronoi volumes " for ((g1,g2), sigs) in conns
+    #=@showprogress 1 "Voronoi volumes "=# for ((g1,g2), sigs) in conns
         a = boundary_area(g1, g2, sigs, P)
         h = norm(P[g1] - P[g2]) / 2
         v = a * h / dim  # Volume computation
