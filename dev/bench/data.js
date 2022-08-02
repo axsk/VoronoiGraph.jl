02aa083363c2e1c9a0ea45ebdffa8b26c8f8e12c
github-action-benchmark <github@users.noreply.github.com>
2022-08-02 15:42:15 +0000

    add Julia benchmark result (julia) benchmark result for f2c07582bf5fe93dca06fd8b6d04ef6417ab73f2

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1659454935917,
+  "repoUrl": "https://github.com/axsk/VoronoiGraph.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "sikorski@zib.de",
+            "name": "Sikorski",
+            "username": "axsk"
+          },
+          "committer": {
+            "email": "sikorski@zib.de",
+            "name": "Sikorski",
+            "username": "axsk"
+          },
+          "distinct": true,
+          "id": "f2c07582bf5fe93dca06fd8b6d04ef6417ab73f2",
+          "message": "add project.toml",
+          "timestamp": "2022-08-02T17:38:44+02:00",
+          "tree_id": "0046126377be7c43fa8fbba40662252045d2d525",
+          "url": "https://github.com/axsk/VoronoiGraph.jl/commit/f2c07582bf5fe93dca06fd8b6d04ef6417ab73f2"
+        },
+        "date": 1659454934277,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "2/100",
+            "value": 2727322.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=664560\nallocs=14508\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "2/1000",
+            "value": 16266384,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=7010528\nallocs=162289\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "3/100",
+            "value": 10000382,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=2108464\nallocs=42680\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "3/1000",
+            "value": 75508216,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=26710640\nallocs=549977\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "4/100",
+            "value": 40660036,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=8268752\nallocs=144408\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "4/1000",
+            "value": 481596776,
+            "unit": "ns",
+            "extra": "gctime=27751228\nmemory=132482096\nallocs=2406841\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "5/100",
+            "value": 200476904.5,
+            "unit": "ns",
+            "extra": "gctime=3449878.5\nmemory=32548864\nallocs=504041\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "5/1000",
+            "value": 3482297640,
+            "unit": "ns",
+            "extra": "gctime=324905602\nmemory=699286720\nallocs=11527234\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":1,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
