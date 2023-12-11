9c4c1c3fefbd66b2f0009eb69184ba2305388ba4
Sikorski <sikorski@zib.de>
2023-12-11 16:09:18 +0100

    update benchmarks

--- scripts/benchmark.jl
+++ scripts/benchmark.jl
@@ -1,44 +1,74 @@
 using BenchmarkTools
+using HighVoronoi
 using VoronoiGraph
-using Random
+using MiniQhull
+using Plots
 
-function bsuite(
-        dims = 2:5,
-        ns = [100, 1000])
-    suite = BenchmarkGroup()
-    for d in dims
-        suite[d] = BenchmarkGroup()
-        for n in ns
-            Random.seed!(1)
-            x = rand(d,n)
-            suite[d][n] = @benchmarkable voronoi($x) seconds=1
-        end
-    end
-    return suite
+function paperplot()
+  b = benchsuite(9)
+  r = @time run(b)
+  try
+    plotperf(r)
+    savefig("bench.png")
+  catch err
+    println("plotting failed with $err:")
+    showerr(stdout, err)
+  end
+  return r
 end
 
-const parameterfile = "../cache/params.json"
-
-function autotune!(suite, load=true)
-    if load && isfile(parameterfile)
-        try
-            println("Loading benchmark parameters")
-            loadparams!(suite, BenchmarkTools.load(parameterfile)[1], :evals, :samples);
-            return
-        catch
-        end
-    end
-    println("Tuning benchmark parameters")
-    tune!(suite)
-    BenchmarkTools.save("params.json", params(suite))
+function fastbench()
+  run(benchsuite(7), samples=10)
 end
 
-function autorun()
-    s = bsuite()
-    autotune!(s)
-    println("Running benchmark")
-    results = run(s, verbose = true)
-    println("Saving benchmark results")
-    BenchmarkTools.save("output.json", median(results))
-    return s, results
+function benchsuite(limit=9)
+  suite = BenchmarkGroup()
+
+  f(x) = [sum(x .^ 2)]
+  f1(x) = sum(x .^ 2)
+
+  for dim in [2, 4, 6]
+    for n in [100, 300, 1_000, 3_000, 10_000, 30_000, 100_000, 300_000]
+      log10(n) + dim > limit && continue
+      local xs = HighVoronoi.VoronoiNodes(rand(dim, n))
+      suite[(dim, n, :HighVoronoi)] = @benchmarkable highvor($xs, integrate=false)
+      suite[(dim, n, :VoronoiGraph)] = @benchmarkable VoronoiGraph.voronoi($xs)
+      suite[(dim, n, :qHull)] = @benchmarkable delaunay($xs)
+      #suite[(dim, n,:HighVoronoiMC)] = @benchmarkable VoronoiGeometry($xs, integrator=VI_MONTECARLO, integrand=$f, mc_accurate=(10, 10, 20), silence=true)
+      #suite[(dim, n,:VoronoiGraphMC)] = @benchmarkable begin
+      #  v = voronoi($xs)
+      #  mc_integrate($f1, v[1], $xs, 10, 10)
+      #end
+    end
+  end
+  return suite
+end
+
+function highvor(xs; kwargs...)
+  try
+    HighVoronoi.VoronoiGeometry(xs, HighVoronoi.Boundary(); silence=true, kwargs...)
+  catch err
+    # still getting errors on HighVoronoi@1.2
+    println("HV errored with $err")
+  end
+end
+
+function plotperf(data_dict)
+  dims = Set(k[1] for k in keys(data_dict))
+  methods = Set(k[3] for k in keys(data_dict))
+  times(x) = median(x).time / 1e9
+  p = plot()
+  linecolor = 1
+  for method in methods
+    label = method
+    for dim in dims
+      t = [times(val) for (k, val) in data_dict if k[1] == dim && k[3] == method]
+      n = [k[2] for (k, _) in data_dict if k[1] == dim && k[3] == method]
+      p = sortperm(n)
+      plot!(n[p], t[p]; label, linecolor, marker=:x, markercolor=linecolor)
+      label = ""
+    end
+    linecolor += 1
+  end
+  plot!(legend=:bottomright, xaxis=:log, yaxis=:log)
 end
